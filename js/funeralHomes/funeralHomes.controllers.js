87ba61551830d17c3b61a06f332762df4bd398cd
Andrew Gonzalez <andrew.gonzalez@gmail.com>
2016-12-14 15:53:54 -0800

    failed at adding Yelp to the front end

--- js/funeralHomes/funeralHomes.controllers.js
+++ js/funeralHomes/funeralHomes.controllers.js
@@ -15,41 +15,18 @@ function FuneralHomeController(FuneralHomeResource, $http) {
   vm.cities = ['Oxnard', 'Santa Paula']
   vm.selectedCity = 'No City Selected'
 
- //  FuneralHomeResource.query().$promise.then(function(data) {
- //   vm.funeralHomes = data;
- // });
- $http.get('https://api.yelp.com/v3/businesses/garcia-mortuary-oxnard', {
-   headers: {
-     'Authorization': 'Bearer DMPhWN0AwnOoL6EVFifWMHH3S2NalpivlG5xFM8Go1cA4mHKHm9hROlxybH-jt-oVc_86l93QZ9NAPwwX0u8M-Y6E3nfTvF4Tx4oLE_9x5k6TQBq9qoAa2dXIZ9RWHYx'
-   }
- }).then(function successCallback(yelpData) {
-   vm.yelpData = yelpData
-   console.log(yelpData)
- }, function errorCallback(response) {
-   console.log(response)
+  FuneralHomeResource.query().$promise.then(function(data) {
+   vm.funeralHomes = data;
  });
-
 
 }
 
 function FuneralHomeShowController(FuneralHomeResource, $stateParams, $http){
   var vm = this;
   vm.funeralHome = {}
-  vm.yelpData = {}
 
   FuneralHomeResource.get({id: $stateParams.id}).$promise.then(function(jsonFuneralHome) {
     vm.funeralHome = jsonFuneralHome;
-  }).then(function() {
-    $http.get('https://api.yelp.com/v3/businesses/' + vm.funeralHome.yelpId, {
-      headers: {
-        Authorization: 'Bearer DMPhWN0AwnOoL6EVFifWMHH3S2NalpivlG5xFM8Go1cA4mHKHm9hROlxybH-jt-oVc_86l93QZ9NAPwwX0u8M-Y6E3nfTvF4Tx4oLE_9x5k6TQBq9qoAa2dXIZ9RWHYx'
-      }
-    }).then(function successCallback(yelpData) {
-      vm.yelpData = yelpData
-      console.log(yelpData)
-    }, function errorCallback(response) {
-      console.log(response)
-    });
   });
 }
 
