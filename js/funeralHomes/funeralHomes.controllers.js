e47ef31f4e0d9e0c37b5abc36ff3001d7e578a12
Andrew Gonzalez <andrew.gonzalez@gmail.com>
2016-12-14 14:44:51 -0800

    stupid testing 3

--- js/funeralHomes/funeralHomes.controllers.js
+++ js/funeralHomes/funeralHomes.controllers.js
@@ -20,7 +20,7 @@ function FuneralHomeController(FuneralHomeResource, $http) {
  // });
  $http.get('https://api.yelp.com/v3/businesses/garcia-mortuary-oxnard', {
    headers: {
-     Authorization: 'Bearer DMPhWN0AwnOoL6EVFifWMHH3S2NalpivlG5xFM8Go1cA4mHKHm9hROlxybH-jt-oVc_86l93QZ9NAPwwX0u8M-Y6E3nfTvF4Tx4oLE_9x5k6TQBq9qoAa2dXIZ9RWHYx'
+     'Authorization': 'Bearer DMPhWN0AwnOoL6EVFifWMHH3S2NalpivlG5xFM8Go1cA4mHKHm9hROlxybH-jt-oVc_86l93QZ9NAPwwX0u8M-Y6E3nfTvF4Tx4oLE_9x5k6TQBq9qoAa2dXIZ9RWHYx'
    }
  }).then(function successCallback(yelpData) {
    vm.yelpData = yelpData
